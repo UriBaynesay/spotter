69a1af4f1d213a7b138d2d8cec7facccda642337
Uri Baynesay <uri552@gmail.com>
2024-12-26 14:54:45 +0200

    first commit

---
+++ components/app-footer.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function AppFooter() {
+  return (
+    <section>
+      <h2 className="text-5xl">Footer</h2>
+    </section>
+  )
+}
+
+export default AppFooter
---
+++ components/app-header.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function AppHeader() {
+  return (
+    <section>
+      <h1 className="text-6xl">Spooter</h1>
+    </section>
+  )
+}
+
+export default AppHeader
